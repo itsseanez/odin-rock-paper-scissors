09284a6851a0cc0a3d7ed88261940aa266ce3cc1
Sean Ezenwa <seanezenwa@gmail.com>
2022-12-13 13:35:51 -0500

    Create index.html and start java logic

---
+++ logic.js
@@ -0,0 +1,18 @@
+function getComputerChoice(computerSelector) {
+    let computerSelection;
+    switch(computerSelector) {
+        case 1:
+            cpuResponse= "Rock";
+            break;
+        case 2:
+            cpuResponse= "Paper";
+            break;
+        case 3:
+            cpuResponse= "Scissors";
+            break;
+    }
+    return computerSelection;
+}
+function playRound(playerSelection, computerSelection) {
+
+}
